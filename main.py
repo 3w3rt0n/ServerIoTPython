7bb43fcc1e6110e064ff2802fff462c04a8012c3
Ewerton <3w3rt0n@gmail.com>
2017-01-08 15:55:44 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 import os
 from flask import Flask
 
-
 app = Flask(__name__)
-
 
 @app.route("/")
 def index():
     return "<h1>Hello World</hi>"
+
+
+@app.route("/<name>")
+def index(name):
+    if name.lower() == "ewerton":
+        return "Olá {}".format(name), 200
+    else:
+        return "Not Found", 404
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
